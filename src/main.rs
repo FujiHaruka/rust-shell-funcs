7cdc4c0d3fabe5a13a16f3a50210daff8887a501
FujiHaruka <fujiharuka@realglobe.jp>
2017-09-09 00:03:08 +0900

    on developping

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+extern crate serde_json;
+
+use std::fs::File;
+use std::io::{Write, Read};
+use std::path::Path;
+use std::process::Command;
+use std::collections::HashMap;
+use serde_json::{Value, Error};
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("data/commands.json").unwrap();
+    let mut commands_json_string = String::new();
+    file.read_to_string(&mut commands_json_string);
+
+    let value: Value = serde_json::from_str(&commands_json_string).unwrap();
+
+    let commands = match value {
+        Value::Array(x) => x,
+        _ => vec![]
+    };
+
+
+    let command = commands.get(0).unwrap();
+    let func = command["func"].as_str().unwrap();
+    let desc = command["desc"].as_str().unwrap();
+
+    exec_func(&func, vec!["hogehoge"]);
+
+    // for command in commands.into_iter() {
+    //     let ref func = command["func"];
+    //     let ref desc = command["desc"];
+    //     println!("{} {}", func, desc);
+    // }
+}
+
+
+fn exec_func (func_str: &str, args: Vec<&str>) {
+    let mut command = func_str.to_string();
+    for i in 0..9 {
+        let var_name = format!("${}", i + 1);
+        let var_value = args.get(i);
+        if let Some(value) = var_value {
+            command = command.replace(&var_name, &value).to_string();
+        }
+    }
+    Command::new("sh").arg("-c").arg(command).spawn().expect("failed");
 }
